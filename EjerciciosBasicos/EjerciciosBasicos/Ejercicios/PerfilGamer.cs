4f7ae5ec1559d9f8894eea16f724f985ecd26ce9
MelanyVanessa <mvanessagonzalez10@gmail.com>
2026-02-23 09:18:23 -0500

    ejercicio

---
+++ EjerciciosBasicos/EjerciciosBasicos/Ejercicios/PerfilGamer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosBasicos.Ejercicios
+{
+    public class PerfilGamer
+    {
+        public static void Ejecutar()
+        {
+            Console.WriteLine("Ingrese su nombre de usuario:");
+            
+        }
+    }
+}
---
+++ EjerciciosBasicos/EjerciciosBasicos/Program.cs
@@ -0,0 +1,17 @@
+﻿using EjerciciosBasicos.Ejercicios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosBasicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PerfilGamer.Ejecutar();
+        }
+    }
+}
