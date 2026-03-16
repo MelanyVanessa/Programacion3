6384b770e766101d7a0785bbb9ec64d773f8773f
MelanyVanessa <mvanessagonzalez10@gmail.com>
2026-03-16 11:46:08 -0500

    ejercicio

---
+++ Creador de Estructura de Proyectos/Creador de Estructura de Proyectos/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Creador_de_Estructura_de_Proyectos
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Ingrese el nombre del proyecto: ");
+                string nombreProyecto = Console.ReadLine();
+
+                
+                string rutaProyecto = Path.Combine(Environment.CurrentDirectory, nombreProyecto);
+                Directory.CreateDirectory(rutaProyecto);
+
+               
+                string documentos = Path.Combine(rutaProyecto, "documentos");
+                string imagenes = Path.Combine(rutaProyecto, "imagenes");
+                string codigo = Path.Combine(rutaProyecto, "codigo");
+
+                Directory.CreateDirectory(documentos);
+                Directory.CreateDirectory(imagenes);
+                Directory.CreateDirectory(codigo);
+
+               
+                Console.WriteLine("Ingrese una breve descripción del proyecto: ");
+                string descripcion = Console.ReadLine();
+
+                string rutaReadme = Path.Combine(documentos, "readme.txt");
+                File.WriteAllText(rutaReadme, descripcion);
+
+                
+                Console.WriteLine("Proyecto creado en: " + Path.GetFullPath(rutaProyecto));
+            }
+        }
+    }
+    
