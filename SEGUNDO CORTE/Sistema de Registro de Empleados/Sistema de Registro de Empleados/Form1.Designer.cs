b049a85adc106597c07476fcc2bc442553172508
MelanyVanessa <mvanessagonzalez10@gmail.com>
2026-04-10 17:38:24 -0500

    Ejercicio

---
+++ SEGUNDO CORTE/Sistema de Registro de Empleados/Sistema de Registro de Empleados/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Sistema_de_Registro_de_Empleados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtIdentificacion = new System.Windows.Forms.TextBox();
+            this.numSueldoBase = new System.Windows.Forms.NumericUpDown();
+            this.cmbDepartamento = new System.Windows.Forms.ComboBox();
+            this.lblResultadoSueldo = new System.Windows.Forms.Label();
+            this.bntRegistrar = new System.Windows.Forms.Button();
+            this.bntLimpiar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numSueldoBase)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Font = new System.Drawing.Font("Lucida Bright", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 160);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Font = new System.Drawing.Font("Lucida Bright", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 206);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 26);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "E-mail :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Font = new System.Drawing.Font("Lucida Bright", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(35, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(181, 26);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Identificacion :";
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombres.Location = new System.Drawing.Point(177, 112);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(315, 30);
+            this.txtNombres.TabIndex = 4;
+            // 
+            // txtApellidos
+            // 
+            this.txtApellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellidos.Location = new System.Drawing.Point(177, 160);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(315, 30);
+            this.txtApellidos.TabIndex = 5;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(154, 202);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(315, 30);
+            this.txtEmail.TabIndex = 6;
+            // 
+            // txtIdentificacion
+            // 
+            this.txtIdentificacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdentificacion.Location = new System.Drawing.Point(233, 249);
+            this.txtIdentificacion.Name = "txtIdentificacion";
+            this.txtIdentificacion.Size = new System.Drawing.Size(315, 30);
+            this.txtIdentificacion.TabIndex = 7;
+            this.txtIdentificacion.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // numSueldoBase
+            // 
+            this.numSueldoBase.Location = new System.Drawing.Point(40, 293);
+            this.numSueldoBase.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numSueldoBase.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numSueldoBase.Name = "numSueldoBase";
+            this.numSueldoBase.Size = new System.Drawing.Size(133, 22);
+            this.numSueldoBase.TabIndex = 8;
+            this.numSueldoBase.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // cmbDepartamento
+            // 
+            this.cmbDepartamento.FormattingEnabled = true;
+            this.cmbDepartamento.Items.AddRange(new object[] {
+            "Ventas",
+            "IT",
+            "Recursos Humanos",
+            "Contabilidad"});
+            this.cmbDepartamento.Location = new System.Drawing.Point(40, 337);
+            this.cmbDepartamento.Name = "cmbDepartamento";
+            this.cmbDepartamento.Size = new System.Drawing.Size(176, 24);
+            this.cmbDepartamento.TabIndex = 9;
+            // 
+            // lblResultadoSueldo
+            // 
+            this.lblResultadoSueldo.AutoSize = true;
+            this.lblResultadoSueldo.Font = new System.Drawing.Font("Lucida Bright", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoSueldo.Location = new System.Drawing.Point(328, 307);
+            this.lblResultadoSueldo.Name = "lblResultadoSueldo";
+            this.lblResultadoSueldo.Size = new System.Drawing.Size(0, 26);
+            this.lblResultadoSueldo.TabIndex = 10;
+            // 
+            // bntRegistrar
+            // 
+            this.bntRegistrar.Font = new System.Drawing.Font("Lucida Bright", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntRegistrar.Location = new System.Drawing.Point(462, 367);
+            this.bntRegistrar.Name = "bntRegistrar";
+            this.bntRegistrar.Size = new System.Drawing.Size(132, 54);
+            this.bntRegistrar.TabIndex = 11;
+            this.bntRegistrar.Text = "Registrar";
+            this.bntRegistrar.UseVisualStyleBackColor = true;
+            this.bntRegistrar.Click += new System.EventHandler(this.bntRegistrar_Click);
+            // 
+            // bntLimpiar
+            // 
+            this.bntLimpiar.Font = new System.Drawing.Font("Lucida Bright", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntLimpiar.Location = new System.Drawing.Point(627, 367);
+            this.bntLimpiar.Name = "bntLimpiar";
+            this.bntLimpiar.Size = new System.Drawing.Size(132, 54);
+            this.bntLimpiar.TabIndex = 12;
+            this.bntLimpiar.Text = "Limpiar";
+            this.bntLimpiar.UseVisualStyleBackColor = true;
+            this.bntLimpiar.Click += new System.EventHandler(this.bntLimpiar_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Bright", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(129, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(522, 34);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Sistema de Registro de Empleados";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.bntLimpiar);
+            this.Controls.Add(this.bntRegistrar);
+            this.Controls.Add(this.lblResultadoSueldo);
+            this.Controls.Add(this.cmbDepartamento);
+            this.Controls.Add(this.numSueldoBase);
+            this.Controls.Add(this.txtIdentificacion);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numSueldoBase)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtIdentificacion;
+        private System.Windows.Forms.NumericUpDown numSueldoBase;
+        private System.Windows.Forms.ComboBox cmbDepartamento;
+        private System.Windows.Forms.Label lblResultadoSueldo;
+        private System.Windows.Forms.Button bntRegistrar;
+        private System.Windows.Forms.Button bntLimpiar;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ SEGUNDO CORTE/Sistema de Registro de Empleados/Sistema de Registro de Empleados/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Sistema_de_Registro_de_Empleados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void bntRegistrar_Click(object sender, EventArgs e)
+        {
+            
+            if (string.IsNullOrWhiteSpace(txtNombres.Text) ||
+                string.IsNullOrWhiteSpace(txtApellidos.Text) ||
+                string.IsNullOrWhiteSpace(txtEmail.Text) ||
+                string.IsNullOrWhiteSpace(txtIdentificacion.Text))
+            {
+                MessageBox.Show("Todos los campos son obligatorios.");
+                return;
+            }
+
+
+            if (!Regex.IsMatch(txtEmail.Text, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                MessageBox.Show("El correo electrónico no es válido.");
+                return;
+            }
+
+            
+            if (!Regex.IsMatch(txtIdentificacion.Text, @"^\d{10}$"))
+            {
+                MessageBox.Show("La identificación debe tener exactamente 10 dígitos.");
+                return;
+            }
+
+            
+            decimal sueldoBase = numSueldoBase.Value;
+            decimal sueldoNeto = sueldoBase - (sueldoBase * 0.10m);
+
+            
+            lblResultadoSueldo.Text = $"Sueldo Neto: {sueldoNeto:C}";
+        }
+
+        
+        
+
+        private void bntLimpiar_Click_1(object sender, EventArgs e)
+        {
+            txtNombres.Clear();
+            txtApellidos.Clear();
+            txtEmail.Clear();
+            txtIdentificacion.Clear();
+            numSueldoBase.Value = numSueldoBase.Minimum;
+            cmbDepartamento.SelectedIndex = -1;
+            lblResultadoSueldo.Text = string.Empty;
+        }
+    }
+}
+
